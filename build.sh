bf89917be06ea8a0c3567c16f035ef8bafc99165
Daniel Sundberg <daniel.sundberg@gmail.com>
2022-03-29 07:04:58 +0000

    keep custom keymap as example only

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ cp build/zephyr/zmk.uf2 ../zmk-config/tbkmini_left.uf2
 west build -p -b nice_nano -- -DSHIELD=tbkmini_right -DZMK_CONFIG="/workspaces/zmk-config/config" && \
 cp build/zephyr/zmk.uf2 ../zmk-config/tbkmini_right.uf2
 
-west build -p -b nice_nano -- -DSHIELD=ahokore -DKEYMAP_FILE="/workspaces/zmk-config/config/ahokore_thumbmods.keymap" -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-cp build/zephyr/zmk.uf2 ../zmk-config/ahokore_thumbmods.uf2
+# west build -p -b nice_nano -- -DSHIELD=ahokore -DKEYMAP_FILE="/workspaces/zmk-config/config/ahokore_thumbmods.keymap" -DZMK_CONFIG="/workspaces/zmk-config/config" && \
+# cp build/zephyr/zmk.uf2 ../zmk-config/ahokore_thumbmods.uf2
 popd
