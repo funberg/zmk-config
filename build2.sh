fe5d394f18f3b97d4e1eeed39dc724ad5f82910a
Daniel Sundberg <daniel.sundberg@king.com>
2023-06-07 11:53:16 +0200

    update build2 script

--- build2.sh
+++ build2.sh
@@ -37,30 +37,12 @@ case "$name" in
         ;;
 esac
 
-cmd="west build -p -b ${board} -- ${extra} -DZMK_CONFIG=\"/workspaces/zmk-config/config\" && \
-cp build/zephyr/zmk.uf2 ../zmk-config/${output}.uf2"
+cmd="west build -p -b ${board} -- ${extra} -DZMK_CONFIG=/workspaces/zmk-config/config"
+cpy="cp build/zephyr/zmk.uf2 ../zmk-config/${output}.uf2"
 
 echo $cmd
 
+# build
 pushd ../app
-${cmd}
+${cmd} && ${cpy}
 popd
-
-
-# west build -p -b nice_nano -- -DSHIELD=ahokore -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-# cp build/zephyr/zmk.uf2 ../zmk-config/ahokore.uf2
-
-# west build -p -b corneish_zen_v2_left -- -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-# cp build/zephyr/zmk.uf2 ../zmk-config/zen_left.uf2
-
-#west build -p -b corneish_zen_v2_right -- -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-#cp build/zephyr/zmk.uf2 ../zmk-config/zen_right.uf2
-
-# west build -p -b nice_nano -- -DSHIELD=tbkmini_left -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-# cp build/zephyr/zmk.uf2 ../zmk-config/tbkmini_left.uf2
-
-# west build -p -b nice_nano -- -DSHIELD=tbkmini_right -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-# cp build/zephyr/zmk.uf2 ../zmk-config/tbkmini_right.uf2
-
-# west build -p -b nice_nano -- -DSHIELD=ahokore -DKEYMAP_FILE="/workspaces/zmk-config/config/ahokore_thumbmods.keymap" -DZMK_CONFIG="/workspaces/zmk-config/config" && \
-# cp build/zephyr/zmk.uf2 ../zmk-config/ahokore_thumbmods.uf2
